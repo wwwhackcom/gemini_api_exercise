f9c2fc09ce5042efde04baa029be548bc2278a95
Nick Wang <wwwhackcom@qq.com>
2024-05-15 00:04:42 +1200

    display opt

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -9,14 +9,16 @@ def do(directory, category):
     csv_path = os.path.join(directory, csv_file)
     quiz_df = pd.read_csv(csv_path)
     
-    # Randomly select a question from the DataFrame
-    random_index = random.randint(0, len(quiz_df) - 1)
-
     # Display the question
-    get_question(random_index, quiz_df)
+    get_question(quiz_df)
 
-def get_question(index, df):    
-    st.write(df.loc[index, 'Questions'])
+def get_question(df):
+    # Randomly select a question from the DataFrame
+    index = random.randint(0, len(df) - 1)
+    st.divider()
+    st.markdown(f"<h6 style='text-align: left; color: black;'>Question: {df.loc[index, 'Questions']}</h6>", unsafe_allow_html=True)
+    st.divider()
     options = ['A', 'B', 'C', 'D']
     for i, option in enumerate(options, start=1):
-        st.write(f"{chr(64+i)}: {df.loc[index, option]}")
+        if not pd.isnull(df.loc[index, option]):
+            st.markdown(f"***{chr(64+i)}: {df.loc[index, option]}***")
