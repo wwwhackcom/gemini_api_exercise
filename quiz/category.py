6d5c58e992b3f9a97267048af562fe481a30ac1c
Nick Wang <wwwhackcom@qq.com>
2024-05-15 21:08:16 +1200

    opt

--- quiz/category.py
+++ quiz/category.py
@@ -4,15 +4,20 @@ from quiz import quiz
 
 def entry():
     # Call the function with the path to your CSV directory
-    display_files_in_directory('quiz/csv/')
+    display_categories()
 
+#@st.cache_data
 def display_files_in_directory(directory):
     # List all files in the directory
     files = os.listdir(directory)
     
     # Filter for CSV files
     categories = [file[:-4] for file in files if file.endswith('.csv')]
+    return categories
 
+def display_categories():
+    directory = 'quiz/csv/'
+    categories = display_files_in_directory(directory)
     st.header("Ice Breaker: Quiz!")
     st.subheader("A collection of trivia, multiple choice questions and answers!")
     st.divider()
