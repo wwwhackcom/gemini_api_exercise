29f50359cf60b75df2181ddc0bb6c430f7026a9d
Nick Wang <wwwhackcom@qq.com>
2024-05-20 17:28:22 +1200

    gemini-1.5-pro

--- generativeai.py
+++ generativeai.py
@@ -6,6 +6,7 @@ GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 genai.configure(api_key = GOOGLE_API_KEY)
 
 def generate_response(prompt):
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-1.5-pro-latest')
+    #model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(prompt)
     return response.text
