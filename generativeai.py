96771ec3809f41c9785ec37f9145aa15b1abb00d
Nick Wang <wwwhackcom@qq.com>
2024-05-21 23:40:39 +1200

    added TOML format secrets

--- generativeai.py
+++ generativeai.py
@@ -1,8 +1,15 @@
 import os
+import streamlit as st
 import google.generativeai as genai
 
+try:
+    GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+except:
+    GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
-GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+if not GOOGLE_API_KEY:
+    st.write("GOOGLE_API_KEY not found")
+
 genai.configure(api_key = GOOGLE_API_KEY)
 
 def generate_response(prompt):
